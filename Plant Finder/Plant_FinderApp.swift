ce5ccfbfc69bf1f342a18d9551652acd0fe9cb14
JaskiratChahal <jaskiratsingh@outlook.com>
2022-05-06 22:07:19 +0100

    Initial Commit

---
+++ Plant Finder/Plant_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Plant_FinderApp.swift
+//  Plant Finder
+//
+//  Created by Jaskirat Chahal on 06/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct Plant_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
